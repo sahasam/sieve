1f0297108c3def789cefdbd40aad5b6e9d41b0be
sahasam <munamalasahas@gmail.com>
2020-09-12 23:25:20 -0500

    add resources module to hold sample filter file

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages
 
 setup(
     name='sieve',
+    packages=find_packages(),
+    package_data={'': ['*.txt']},
     version='1.0.0',
     author='Sahas Munamala',
     author_email='munamalasahas@gmail.com',
-    packages=find_packages(),
     url='https://github.com/sahasam/sieve-organizer',
     license='MIT License',
     descrption='Command line tool for organizing files',
